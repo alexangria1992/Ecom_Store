e41ebe1e7cd6c3d4412a53dc1b37967f381260d9
alexangria1992 <alex040892@gmail.com>
2020-08-24 16:01:03 +1200

    Created navigation markup and styling

--- index.php
+++ index.php
@@ -4,10 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Commerce</title>
+    <link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100" rel="stylesheet">
     <link href="styles/bootstrap.min.css" rel="stylesheet">
+    <link href="styles/style.css" rel="stylesheet">
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
+    <div id="top">
+        <div class="container">
+            <div class="col-md-6 offer">
+                <a href="" class="btn btn-success btn-sm">
+                    Welcome: Guest
+                </a>
+                <a href="">
+                    Shopping Cart Total Price: $100, Total Items: 2
+                </a>
+            </div>
+            <div class="col-md-12">
+                <ul class="menu">
+                    <li>
+                        <a href="customer_register.php">Register</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">My Account</a>
+                    </li>
+                    <li>
+                        <a href="cart.php">Go to Cart</a>
+                    </li>
+                    <li>
+                        <a href="login.php">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
 
 
     <script src="js/jquery.min.js"></script>
